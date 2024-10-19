ee74a914c2f26bda2c358bf3cad9680e633db585
vinodpatil2002 <vinodnspatil@gmail.com>
2024-10-19 20:19:45 +0530

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,30 @@
 import type { Config } from "tailwindcss";
 
+const {fontFamily} = require("tailwindcss/defaultTheme");
+
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+			fontFamily: {
+				sans: ["var(--font-sans)", ...fontFamily.sans],
+			},
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [require("tailwindcss-animate")],
 };
 export default config;
