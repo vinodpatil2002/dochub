e11a665193ab1e9b3b3036f27a7d5ba124c9e631
vinodpatil2002 <vinodnspatil@gmail.com>
2024-10-30 23:01:13 +0530

    Setup Sentry for live error debuging

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://abf9ed900d994134250e35594d2e1baf@o4508212441251840.ingest.de.sentry.io/4508212744159312",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
